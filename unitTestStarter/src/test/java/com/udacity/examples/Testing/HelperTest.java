829351c492bf014436b66c1aba4fd64db2010169
Foystor <foystor@gmail.com>
2022-08-23 15:54:50 +0800

    test: Add test for getMergedList method

--- unitTestStarter/src/test/java/com/udacity/examples/Testing/HelperTest.java
+++ unitTestStarter/src/test/java/com/udacity/examples/Testing/HelperTest.java
@@ -1,8 +1,19 @@
 package com.udacity.examples.Testing;
 
-import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class HelperTest {
 
+    @Test
+    public void verify_getMergedList() {
+        List<String> empName = Arrays.asList("Amy", "", "Alice", "", "Mery");
+        String expectedNames = "Amy, Alice, Mery";
+        String names = Helper.getMergedList(empName);
+        Assert.assertEquals(expectedNames, names);
+    }
 	
 }
